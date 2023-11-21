402befb3ea25373a526cec1168f488b3d67b8534
Hezekiah Adeusi <hezekiaha58@gmail.com>
2023-11-21 15:21:59 +0000

    Initial commit:
    - Installed Jest
    - Created functions in calculator.js to be tested

---
+++ calculator.js
@@ -0,0 +1,37 @@
+const sum = function (a, b) {
+    return a + b;
+};
+
+const subtract = (a, b) => {
+    return a - b;
+};
+
+const multiply = (a, b) => {
+    return a * b;
+};
+
+const divide = (a, b) => {
+    return a / b;
+};
+
+const modulus = (a, b) => {
+    return a % b;
+};
+
+const even = (number) => {
+    return number % 2 === 0;
+};
+
+const odd = (number) => {
+    return number & 2 !== 0;
+};
+
+module.exports = {
+    sum,
+    subtract,
+    multiply,
+    divide,
+    modulus,
+    even,
+    odd
+};
